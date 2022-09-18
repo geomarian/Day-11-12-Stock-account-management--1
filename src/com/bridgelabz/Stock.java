8a1ab7a51ef4a69598204299c4147bdba2d47501
geomarian <geomarian520@gmail.com>
2022-09-18 23:13:59 +0530

    Added: Stock File

---
+++ src/com/bridgelabz/Stock.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author ASUS
+ *   List of Stocks
+ *Stock Names, Number of Share, Share Price.
+Print a Stock Report with the total value of each Stock and the total value of
+Stock.
+ */
+public class Stock extends StocksMain {
+
+
+	public void displayList() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("List of Stocks are:");
+		System.out.println("TATA MOTORS");
+		System.out.println("Relaince");
+		System.out.println("Tesla");
+	}
+
+}
